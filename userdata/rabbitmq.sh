21138df2b08709b53997c3ca082baa08af9e9ed2
egnicolas <egloff.ni@gmail.com>
2025-08-28 15:18:39 +0200

    release: final version

---
+++ userdata/rabbitmq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install rabbitmq-server -y
+sudo apt-get install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+firewall-cmd --add-port=5672/tcp
+firewall-cmd --runtime-to-permanent
+sudo systemctl start rabbitmq-server
+sudo systemctl enable rabbitmq-server
+sudo systemctl status rabbitmq-server
+sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
+sudo rabbitmqctl add_user test test
+sudo rabbitmqctl set_user_tags test administrator
+sudo systemctl restart rabbitmq-server
